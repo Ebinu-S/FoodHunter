bbca8152b4124bda2b0d6ce88656651890e05288
dr-retr0-o <ebinusuneer2nd@gmail.com>
2020-04-19 21:39:16 +0530

    fifth commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   get 'recipes/index'
-  root 'books#index'
+  root 'recipes#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
